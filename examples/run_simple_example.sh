0e51ac59efcbe0c007f0aafa037fd79173cdb46c
alexis arnaudon <alexis.arnaudon@epfl.ch>
2021-02-11 22:31:22 +0100

    update pypi

--- examples/run_simple_example.sh
+++ examples/run_simple_example.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/bin/bash
 
 python create_graph.py
@@ -14,6 +15,12 @@ pygenstability run \
 #    sbm_graph.pkl
 
 pygenstability plot_scan --help
+=======
+#!/bin/zsh
+
+python create_graph.py
+pygenstability run --n-time 100 sbm_graph.pkl
+>>>>>>> Stashed changes
 pygenstability plot_scan results.pkl
 
 pygenstability plot_communities --help
