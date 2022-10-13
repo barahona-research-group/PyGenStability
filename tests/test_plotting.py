4db48582b3af42c6eb65cc7e4eca1b0c450260dc
arnaudon <alexis.arnaudon@epfl.ch>
2022-10-13 11:48:11 +0200

    fix path

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,4 +1,5 @@
 """Test plotting module."""
+from pathlib import Path
 from diff_pdf_visually import pdfdiff
 import filecmp
 
@@ -6,14 +7,16 @@ from pygenstability import plotting
 from pygenstability.constructors import load_constructor
 from pygenstability import pygenstability as pgs
 
+DATA = Path(__file__).absolute().parent / "data"
+
 
 def test_plot_scan(graph, tmp_path):
     constructor = load_constructor("continuous_combinatorial", graph)
     results = pgs.run(graph, constructor=constructor)
     plotting.plot_scan(results, figure_name=tmp_path / "scan.pdf")
-    pdfdiff("data/scan.pdf", tmp_path / "scan.pdf")
+    pdfdiff(DATA / "scan.pdf", tmp_path / "scan.pdf")
 
     plotting.plot_scan(
         results, use_plotly=True, live=False, plotly_filename=str(tmp_path / "scan.html")
     )
-    filecmp.cmp("data/scan.html", tmp_path / "scan.html")
+    filecmp.cmp(DATA / "scan.html", tmp_path / "scan.html")
