0fde1365fceded6823ba74ef848c8d11f5b6bb70
arnaudon <alexis.arnaudon@epfl.ch>
2022-10-13 15:30:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pybind11.setup_helpers import Pybind11Extension
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 ext_modules = [
     Pybind11Extension(
