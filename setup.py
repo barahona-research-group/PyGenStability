c4c40f2cb58158581a2d61575bc4a2eee4bac51a
arnaudon <alexis.arnaudon@epfl.ch>
2023-05-05 09:03:31 +0200

    Submodules in actions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pybind11.setup_helpers import Pybind11Extension
 from setuptools import find_namespace_packages
 from setuptools import setup
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 ext_modules = [
     Pybind11Extension(
