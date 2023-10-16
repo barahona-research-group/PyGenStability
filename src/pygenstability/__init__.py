a853bae28db9ed277b2dfe0787ff5fe8e4d4c89b
arnaudon <alexis.arnaudon@epfl.ch>
2023-10-16 15:40:16 +0200

    Simplify imports

--- src/pygenstability/__init__.py
+++ src/pygenstability/__init__.py
@@ -2,3 +2,7 @@
 from pygenstability.io import load_results
 from pygenstability.io import save_results
 from pygenstability.pygenstability import run
+from pygenstability.pygenstability import evaluate_NVI
+from pygenstability.plotting import *
+from pygenstability.optimal_scales import identify_optimal_scales
+
