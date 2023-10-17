90a9fd480e3daf85a11af7059d820bd53bc0aa1d
arnaudon <alexis.arnaudon@epfl.ch>
2023-10-17 10:43:44 +0200

    lint

--- src/pygenstability/__init__.py
+++ src/pygenstability/__init__.py
@@ -1,9 +1,8 @@
 """Import main functions."""
+from pygenstability.constructors import *
 from pygenstability.io import load_results
 from pygenstability.io import save_results
-from pygenstability.pygenstability import run
-from pygenstability.pygenstability import evaluate_NVI
-from pygenstability.plotting import *
-from pygenstability.constructors import *
 from pygenstability.optimal_scales import identify_optimal_scales
-
+from pygenstability.plotting import *
+from pygenstability.pygenstability import evaluate_NVI
+from pygenstability.pygenstability import run
