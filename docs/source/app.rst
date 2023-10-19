de5951eca3eefc70cb49c1c81386c73e107ae315
arnaudon <alexis.arnaudon@epfl.ch>
2023-10-19 09:34:20 +0200

    rebuilt docs

--- docs/source/app.rst
+++
@@ -1,9 +0,0 @@
-The cli app
-===========
-
-.. automodule:: pygenstability.app
-   :members:
-
-.. click:: pygenstability.app:cli
-  :prog: cli
-  :show-nested:
--- src/pygenstability/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Import main functions."""
-from pygenstability.io import load_results
-from pygenstability.io import save_results
-from pygenstability.pygenstability import run
