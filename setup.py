5d750c48d71391f745cbadacc2c59ddd87c10a2b
arnaudon <alexis.arnaudon@epfl.ch>
2024-04-29 14:34:24 +0200

    0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pybind11.setup_helpers import Pybind11Extension
 from setuptools import find_namespace_packages
 from setuptools import setup
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 ext_modules = [
     Pybind11Extension(
